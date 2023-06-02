57428c1aed12388e6cb16067d2fcb1593062421d
Dmitry Lutsishin <graf.luck@gmail.com>
2023-06-02 10:45:18 +0300

    реализовал перерисовывание информации о маршруте при изменении модели

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -1,13 +1,13 @@
 import TripInfoView from '../view/trip-info-view.js';
-import { render, RenderPosition } from '../framework/render.js';
+import { render, remove, RenderPosition } from '../framework/render.js';
+import { UpdateType, UserAction } from '../const.js';
 
 export default class TripInfoPresenter {
   #tripInfoContainer = null;
+  #tripInfoComponent = null;
   #pointsModel = null;
   #destinationsModel = null;
   #offersModel = null;
-
-  #routePoints = [];
 
   constructor(tripInfoContainer, models) {
     this.#tripInfoContainer = tripInfoContainer;
@@ -16,34 +16,80 @@ export default class TripInfoPresenter {
     this.#pointsModel = pointsModel;
     this.#destinationsModel = destinationsModel;
     this.#offersModel = offersModel;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
+  }
+
+  get #points() {
+    return this.#pointsModel.points;
+  }
+
+  get #tripStartDate() {
+    return this.#pointsModel.tripStartDate;
+  }
+
+  get #tripEndDate() {
+    return this.#pointsModel.tripEndDate;
+  }
+
+  get #totalPrice() {
+    return this.#pointsModel.totalPrice;
+  }
+
+  get #destinationIds() {
+    return this.#pointsModel.destinationIds;
+  }
+
+  get #routeOfTrip() {
+    return this.#destinationsModel.getRouteOfTrip(this.#destinationIds);
   }
 
   init() {
-    this.#routePoints = [...this.#pointsModel.points];
-    const tripStartDate = this.#pointsModel.tripStartDate;
-    const tripEndDate = this.#pointsModel.tripEndDate;
-
-    const totalPrice = this.#calculateTotalPrice(this.#pointsModel.totalPrice);
-    const routeOfTrip = this.#destinationsModel.getRouteOfTrip(this.#pointsModel.destinationIds);
+    const totalPrice = this.#calculateTotalPrice(this.#totalPrice);
 
     this.#renderTripInfo({
-      tripStartDate: tripStartDate,
-      tripEndDate: tripEndDate,
+      tripStartDate: this.#tripStartDate,
+      tripEndDate: this.#tripEndDate,
       totalPrice: totalPrice,
-      routeOfTrip: routeOfTrip,
+      routeOfTrip: this.#routeOfTrip,
     });
   }
 
+  #clearTripInfo() {
+    remove(this.#tripInfoComponent);
+  }
+
   #renderTripInfo(data) {
-    const tripInfoComponent = new TripInfoView(data);
-    render(tripInfoComponent, this.#tripInfoContainer, RenderPosition.AFTERBEGIN);
+    this.#tripInfoComponent = new TripInfoView(data);
+    render(this.#tripInfoComponent, this.#tripInfoContainer, RenderPosition.AFTERBEGIN);
   }
 
   #calculateTotalPrice(pointsBasePrice) {
     let offersPrice = 0;
-    for (const point of this.#routePoints) {
-      offersPrice += this.#offersModel.getTotalPriceByTypeAndIds(point.type, point.offers);
+    for (const point of this.#points) {
+      offersPrice += this.#getTotalPriceByTypeAndIds(point);
     }
     return pointsBasePrice + offersPrice;
   }
+
+  #getTotalPriceByTypeAndIds(point) {
+    return this.#offersModel.getTotalPriceByTypeAndIds(point.type, point.offers);
+  }
+
+  #handleModelEvent = (updateType, point) => {
+    switch (updateType) {
+      case UpdateType.PATCH:
+        // - обновить часть списка (например, когда поменялось описание)
+
+        break;
+      case UpdateType.MINOR:
+        // - обновить список (например, когда задача ушла в архив)
+        break;
+      case UpdateType.MAJOR:
+        // - обновить всю доску (например, при переключении фильтра)
+        this.#clearTripInfo();
+        this.init();
+        break;
+    }
+  };
 }
