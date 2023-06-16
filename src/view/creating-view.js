2eafa24ba18017391a1e0dafc084eba035fe366b
Dmitry Lutsishin <graf.luck@gmail.com>
2023-06-16 09:15:16 +0300

    реализовал блокировку формы при сетевых запросах

--- src/view/creating-view.js
+++ src/view/creating-view.js
@@ -35,14 +35,14 @@ const createDestinationNamesInTemplate = (destinationsName) => {
   return list;
 };
 
-const createListOfOffersInTemplate = (offersForType, checkedOffers) => {
+const createListOfOffersInTemplate = (offersForType, checkedOffers, isDisabled) => {
   let list = '';
   for (const offer of offersForType) {
     const lastWordOfTitle = offer.title.split(' ').pop();
     const isChecked = checkedOffers?.find((checkedOffer) => checkedOffer.id === offer.id);
     list +=
       `<div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${lastWordOfTitle}-1" type="checkbox" name="event-offer-${lastWordOfTitle}" ${isChecked ? 'checked' : ''}>
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${lastWordOfTitle}-1" type="checkbox" name="event-offer-${lastWordOfTitle}" ${isChecked ? 'checked' : ''} ${isDisabled ? 'disabled' : ''}>
         <label class="event__offer-label" for="event-offer-${lastWordOfTitle}-1">
           <span class="event__offer-title">${offer.title}</span>
           &plus;&euro;&nbsp;
@@ -53,13 +53,13 @@ const createListOfOffersInTemplate = (offersForType, checkedOffers) => {
   return list;
 };
 
-const createSectionOfOffersInTemplate = (offersForType, checkedOffers) => {
+const createSectionOfOffersInTemplate = (offersForType, checkedOffers, isDisabled) => {
   if (offersForType !== undefined) {
     return `
       <section class="event__section  event__section--offers">
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
         <div class="event__available-offers">
-          ${createListOfOffersInTemplate(offersForType, checkedOffers)}
+          ${createListOfOffersInTemplate(offersForType, checkedOffers, isDisabled)}
         </div>
       </section>`;
   }
@@ -107,7 +107,7 @@ function createPointTemplate(point, destinationsName) {
                     <span class="visually-hidden">Choose event type</span>
                     <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
                   </label>
-                  <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+                  <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox" ${isDisabled ? 'disabled' : ''}>
                   <div class="event__type-list">
                     <fieldset class="event__type-group">
                       <legend class="visually-hidden">Event type</legend>
@@ -118,7 +118,7 @@ function createPointTemplate(point, destinationsName) {
 
                 <div class="event__field-group  event__field-group--destination">
                   <label class="event__label  event__type-output" for="event-destination-1">${type}</label>
-                  <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination ? destination.name : ''}" list="destination-list-1">
+                  <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination ? destination.name : ''}" list="destination-list-1" ${isDisabled ? 'disabled' : ''}>
                   <datalist id="destination-list-1">
                     ${createDestinationNamesInTemplate(destinationsName)}
                   </datalist>
@@ -126,10 +126,10 @@ function createPointTemplate(point, destinationsName) {
 
                 <div class="event__field-group  event__field-group--time">
                   <label class="visually-hidden" for="event-start-time-1">From</label>
-                  <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formatDate(dateFrom, DateFormat.FULL_DATE)}">
+                  <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formatDate(dateFrom, DateFormat.FULL_DATE)}" ${isDisabled ? 'disabled' : ''}>
                   &mdash;
                   <label class="visually-hidden" for="event-end-time-1">To</label>
-                  <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formatDate(dateTo, DateFormat.FULL_DATE)}">
+                  <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formatDate(dateTo, DateFormat.FULL_DATE)}" ${isDisabled ? 'disabled' : ''}>
                 </div>
 
                 <div class="event__field-group  event__field-group--price">
@@ -137,14 +137,14 @@ function createPointTemplate(point, destinationsName) {
                     <span class="visually-hidden">Price</span>
                     &euro;
                   </label>
-                  <input class="event__input  event__input--price" id="event-price-1" type="number" min="0" name="event-price" value="${basePrice}">
+                  <input class="event__input  event__input--price" id="event-price-1" type="number" min="0" name="event-price" value="${basePrice}" ${isDisabled ? 'disabled' : ''}>
                 </div>
 
-                <button class="event__save-btn  btn  btn--blue" type="submit">${isSaving ? 'Saving...' : 'Save'}</button>
-                <button class="event__reset-btn" type="reset">${buttonNegativeText(false, isDeleting)}</button>
+                <button class="event__save-btn  btn  btn--blue" type="submit" ${isDisabled ? 'disabled' : ''}>${isSaving ? 'Saving...' : 'Save'}</button>
+                <button class="event__reset-btn" type="reset" ${isDisabled ? 'disabled' : ''}>${buttonNegativeText(false, isDeleting)}</button>
               </header>
               <section class="event__details">
-                ${createSectionOfOffersInTemplate(offersForType, offers)}
+                ${createSectionOfOffersInTemplate(offersForType, offers, isDisabled)}
                 ${createSectionOfDestinationInTemplate(destination)}
               </section>
             </form>
