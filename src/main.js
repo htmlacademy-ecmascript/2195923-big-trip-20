49a2a554ad0a68bb6876d416967d600a4e46d809
Dmitry Lutsishin <graf.luck@gmail.com>
2023-08-11 08:00:34 +0300

    поправил шрифт проекта

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import TripPresenter from './presenter/trip-presenter.js';
 
 const body = document.querySelector('body');
+body.classList.add('page-body');
 
 const tripPresenter = new TripPresenter({tripContainer: body});
 tripPresenter.init();
